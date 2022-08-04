1516ea1cdd7da2e15e2f6c46a7b2c579b0f4fade
Masatoshi N <nishiguchi.masa@gmail.com>
2022-08-04 15:44:00 -0400

    Hello custom mdc button

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const autoprefixer = require('autoprefixer')
+
 // https://material.io/develop/web/getting-started
 module.exports = [
   {
@@ -22,13 +24,24 @@ module.exports = [
             { loader: 'extract-loader' },
             { loader: 'css-loader' },
             {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer()]
+                }
+              }
+            },
+            {
               loader: 'sass-loader',
               options: {
                 // Prefer Dart Sass
                 implementation: require('sass'),
 
                 // See https://github.com/webpack-contrib/sass-loader/issues/804
-                webpackImporter: false
+                webpackImporter: false,
+                sassOptions: {
+                  includePaths: ['./node_modules']
+                }
               }
             }
           ]
